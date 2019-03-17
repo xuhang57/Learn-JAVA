0e1d9df59e544096a45be056b5ec6c02a6a865a0
xuhang57 <xuhang57@users.noreply.github.com>
2019-03-17 12:55:23 -0400

    add binary search in algs4 and useargument in itp

---
+++ Algs4/README.md
@@ -0,0 +1,5 @@
+# Algorithms 4th Edition
+
+Codes and texts are from: https://algs4.cs.princeton.edu/
+
+These might have some modifications due to personal preferences
---
+++ IntroToProgramming/ElementOfProgramming/UseArgument.java
@@ -0,0 +1,13 @@
+/*
+ * Compilation: javac UseArugment.java
+ * Execution: java UseArgument yourname
+ *
+ */
+
+public class UseArgument {
+	public static void main(String[] args) {
+		System.out.print("Hi, ");
+		System.out.print(args[0]);
+		System.out.println(". How are you?");
+	}
+}	
