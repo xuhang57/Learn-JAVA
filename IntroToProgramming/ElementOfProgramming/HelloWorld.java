af12d444f95b7bef3079b06d055deb0b2a41fd62
xuhang57 <xuhang57@users.noreply.github.com>
2019-03-16 21:44:39 -0400

    add hello world

---
+++ IntroToProgramming/ElementOfProgramming/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+ * Complilation: javac HelloWorld.java
+ * Execution: java HelloWorld
+ *
+ */
+
+ public class HelloWorld {
+	 public static void main(String[] args) {
+		 System.out.println("Hello, World!");
+	 }
+ }
