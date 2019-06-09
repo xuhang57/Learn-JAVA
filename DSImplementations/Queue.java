b207615eb7a736bc2ad713f067aed90f9ed61191
xuhang57 <xuhang57@users.noreply.github.com>
2019-06-09 17:01:55 -0400

    add ds implementation queue

---
+++ DSImplementations/Queue.java
@@ -0,0 +1,52 @@
+/* Implement a Queue with using LinkedList (singly) */
+public class ListNode {
+    int value;
+    ListNode next;
+    public ListNode(int v) {
+        value = v;
+    }
+}
+
+class myQueue {
+    private ListNode head;
+    private ListNode tail;
+    
+    public myQueue() {
+        head = null;
+        tail = null;
+    }
+
+    //   1 -> 2 -> 3 -> 4
+    //   H              T
+    public void offer(int val) {
+        if (head == null) {
+            head = new ListNode(val);
+            tail = head;
+        } else {
+            tail.next = new ListNode(val);
+            tail = tail.next;
+        }
+    }
+
+    public Integer poll() {
+        if (head == null) {
+            return null;
+        }
+        ListNode firstNode = head;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        // Best Practice: clean up the node next pointer
+        firstNode.next = null;
+        return firstNode.val;
+    }
+
+    public Integer peek() {
+        if (head == null) {
+            return null;
+        }
+        
+        return head.val;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Learn-JAVA
+
+## Stack
+
+* [Why should I use Deque over Stack?](https://stackoverflow.com/questions/12524826/why-should-i-use-deque-over-stack)
+* [Why is ArrayDeque better than LinkedList?](https://stackoverflow.com/questions/6163166/why-is-arraydeque-better-than-linkedlist)
+
+## Queue
+
+* [What is the Fastest Java Collection with the Basic Functionality of a Queue?](https://stackoverflow.com/questions/6129805/what-is-the-fastest-java-collection-with-the-basic-functionality-of-a-queue)
